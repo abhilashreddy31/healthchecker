ff5bb772a6ebb342d44a251c45a4a1431c20463d
abhilashreddy31 <kanamathareddyabhilash6@gmail.com>
2023-06-30 20:52:55 +0530

    changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
-module.exports = {
-  images: {
-    loader: 'imgix',
-    path: '/',
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    "images": {
+    unoptimized: true,
   },
 }
 
+module.exports = nextConfig
